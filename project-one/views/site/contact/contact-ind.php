191c73183fc9aa7ca432797d8d6f7fb10c07af07
Alan <alan_souza_nonijana@hotmail.com.br>
2020-09-11 02:03:57 -0300

    pagina 'quem-somos' e pagina 'posts' criadas

---
+++ project-one/views/site/contact/contact-ind.php
@@ -0,0 +1,54 @@
+<div class="contact" id="content-contact">
+	
+	<section>
+		
+		<div class="container">
+
+			<h2 class="text-center m-5" style="text-decoration: underline;">Preencha o formulário para enviar a mensagem!</h2><br>
+
+			
+			<div class="row">
+				
+				<!-- Formulário -->
+				<div class="col-md-6 col-sm-12 col-12">
+					
+					<form name="formContact">
+						
+						<div class="form-group">
+							<label>Nome</label><br>
+							<input class="form-control" type="text" name="name" placeholder="Digite seu nome completo.">
+						</div>
+
+						<div class="form-group">
+							<label>E-mail</label><br>
+							<input class="form-control" type="email" name="email" placeholder="Digite seu E-mail.">
+						</div>
+
+						<div class="form-group">
+							<label>Telefone</label><br>
+							<input class="form-control" type="text" name="phone" placeholder="Digite seu número de telefone.">
+						</div>
+
+						<div class="form-group mb-5">
+							<label>Mensagem</label><br>
+							<textarea class="form-control" name="message" placeholder="Deixe o seu motivo para contato"></textarea>
+						</div>
+
+						<div class="form-group">
+							<div class="btn btn-secondary">Enviar</div>
+						</div>
+
+					</form>
+
+				</div>
+
+				<!-- Endereço -->
+				<div class="col-md-6 col-sm-12 col-12"></div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+</div>
---
+++ project-one/views/site/posts/post-ind.php
@@ -0,0 +1,21 @@
+<div class="posts" id="content-posts">
+	
+	<section>
+		
+		<div class="container">
+			
+			<h2 class="text-center m-5" style="text-decoration: underline;">Imagens de apoio</h2>
+
+			<div class="row">
+				
+				<div class="col-12">
+					<img class="m-5 border border-dark img-thumbnail" src="<?php echo $url; ?>/assets/img/help1.png">
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+</div>
